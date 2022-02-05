41e7af874daedd41889c9b5937d93952988784d8
NeskoDorc <nebojsa85.milosevic@gmail.comm>
2022-02-05 13:21:24 +0100

    Create mig,cont,mod

---
+++ database/migrations/2022_02_05_105403_create_parts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supplier_id')->nullable();
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+            $table->string('part_desc');
+            $table->string('category');
+            $table->string('part_number');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('priority');
+            $table->integer('days_valid');
+
+            $table->string('condition');
+
+
+
+
+
+
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+}
